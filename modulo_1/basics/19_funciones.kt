dd59ee1c8a01694315afd2a94a0325004979a1dc
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-16 11:02:40 -0500

    Clase 16-04-2026

---
+++ modulo_1/basics/19_funciones.kt
@@ -0,0 +1,28 @@
+fun main() {
+    println("Funciones")
+    val saludo = saludo()
+    println("$saludo")
+    val suma: Int = sumar(5,4)
+    println(suma)
+    print("${restarTipoExpresion(5,3)}")
+    print(restarTipoExpresion(5,3))
+    print(restarTipoInferi(5,3))
+    saludar("Pedro")
+}
+
+fun saludo(): String{
+    return "Hello"
+}
+
+fun sumar(a: Int,b: Int):Int{
+    return a+b
+}
+
+//tipo expresion
+fun restarTipoExpresion(a: Int,b:Int): Int=a-b
+//inferido
+fun restarTipoInferido(a: Int,b:Int)=a-b
+
+fun saludar(nombre: String){
+    println("Hola $nombre")
+}
---
+++ modulo_1/basics/21_set.kt
@@ -0,0 +1,28 @@
+fun main() {
+  println("Set")
+    val numeros = setOf(1,2,3,4,8,4,1,3)
+    println("Numeros Set: ${numeros}")
+    println("Operaciones de conjuntos")
+    val pares = setOf(2,4,6,8,10)
+    val multiplos3 = setOf(3,6,9,12)
+    println("Pares set: ${pares}")
+    println("Multiplos de 3 set: ${multiplos3}")
+    println("Union: ${pares union multiplos3}")
+    println("Intersección: ${pares intersect multiplos3}")
+    println("Substraccion: ${pares subtract multiplos3}")
+    println("Pares set: ${pares}")
+    println("Multiplos 3 set: ${multiplos3}")
+    
+    
+    println("Set mutable")
+    val tags=mutableSetOf("kotlin", "java", "reactnative")
+    println(tags)
+    tags.add("kotlin")
+    println(tags)
+    tags.add("javascript")
+    println(tags)
+    tags.remove("java")
+    println(tags)
+    println("Verificar si el elemento existe: ${"kotlin" in tags}")
+    println("Verificar si el elemento existe: ${"java" in tags}")
+}
