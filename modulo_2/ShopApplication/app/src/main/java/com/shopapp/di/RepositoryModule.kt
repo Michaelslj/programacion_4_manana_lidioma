9b3808f6aa0745d82d2c9550deffd84ead9cc48d
Michael Lidioma <michaellidioma45@gmail.com>
2026-05-28 05:03:40 -0500

    03_Register-Login

---
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.shopapp.di
+
+import com.shopapp.data.repository.AuthRepositoryImpl
+import com.shopapp.data.repository.CategoryRepositoryImpl
+import com.shopapp.domain.repository.AuthRepository
+import com.shopapp.domain.repository.CategoryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds @Singleton
+    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
+
+    @Binds @Singleton
+    abstract fun bindCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository
+}
