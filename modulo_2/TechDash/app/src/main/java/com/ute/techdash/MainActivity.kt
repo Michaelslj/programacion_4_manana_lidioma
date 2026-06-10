8cc05f382397b90f37a6af0c9f548ec08f408529
Aliii666 <aliyhamy04@gmail.com>
2026-06-10 06:15:30 -0500

    Clase

--- modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivity.kt
+++ modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivity.kt
@@ -4,7 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.ute.techdash.ui.multimedia.PantallaMultimedia
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavOptionsBuilder
+import com.ute.techdash.ui.hardware.DashboardHardware
+import com.ute.techdash.ui.hardware.PantallaMenu
+import com.ute.techdash.ui.hardware.gps.PantallaGPS
+import com.ute.techdash.ui.hardware.sensores.PantallaSensores
+import com.ute.techdash.ui.multimedia.PantallaCamara
+import com.ute.techdash.ui.permisos.PantallaPermisos
 import com.ute.techdash.ui.theme.TechDashTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +22,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TechDashTheme {
-                PantallaMultimedia()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "permisos") {
+                    composable("permisos")  {
+                        PantallaPermisos(
+                            onTodosConcedidos = {
+                                navController.navigate("menu") {
+                                    popUpTo("permisos") { inclusive = true }
+                                }
+                            }
+                        )
+                    }
+                    composable("menu")      { PantallaMenu(navController) }
+                    composable("gps")       { PantallaGPS() }
+                    composable("sensores")  { PantallaSensores() }
+                    composable("dashboard") { DashboardHardware() }
+                    composable("camara")    {
+                        PantallaCamara(
+                            onFotoTomada = { uri ->
+                                println("Foto guardada en: $uri")
+                                navController.popBackStack()
+                            },
+                            onCerrar = { navController.popBackStack() }
+                        )
+                    }
+                }
             }
         }
     }
